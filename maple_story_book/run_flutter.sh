d9f741535291ecab8fe250d5330a48b74f996fbe
dohy-9443 <dong_hyun95@hanmail.net>
2024-09-17 22:05:21 +0900

    shell script add

---
+++ maple_story_book/run_flutter.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# 쉘 스크립트 실행 권한 부여 명령어
+# 해당 프로젝트 root 위치에서 명령어 사용
+# chmod +x run_flutter.sh
+
+# FVM 으로 Flutter 패키지 설치
+#fvm flutter pub get
+
+# Flutter 패키지 설치
+#flutter pub get
+
+# FVM 으로 Flutter 패키지 업데이트
+#fvm flutter pub update
+
+# Flutter 패키지 업데이트
+#flutter pub update
+
+# FVM 으로 Build Runner 파일 생성
+#fvm flutter pub run build_runner build --delete-conflicting-outputs
+
+# Build Runner 파일 생성
+#flutter pub run build_runner build --delete-conflicting-outputs
+
+# FVM 으로 Build Runner 파일 지속적으로 실행
+#fvm flutter pub run build_runner watch --delete-conflicting-outputs
+
+# Build Runner 파일 지속적으로 실행
+#flutter pub run build_runner watch --delete-conflicting-outputs
+
+# FVM 으로 앱 실행
+#fvm flutter run
+
+# 앱 실행
+#flutter run
