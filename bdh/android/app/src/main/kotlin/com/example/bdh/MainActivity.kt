5f769d601c59ffab2a4ac0baf4d55840418214d8
dohy-9443 <dong_hyun95@hanmail.net>
2024-09-17 19:01:58 +0900

    delete

--- bdh/android/app/src/main/kotlin/com/example/bdh/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bdh
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- sht/android/app/src/main/kotlin/com/example/sht_subject/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sht_subject
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
