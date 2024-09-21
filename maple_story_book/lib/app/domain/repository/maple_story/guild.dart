5c06b635ad362ed13b321c61d53a5de3e31959be
dohy-9443 <dong_hyun95@hanmail.net>
2024-09-21 22:56:30 +0900

    feat : IGuildRepository 작성

--- maple_story_book/lib/app/domain/repository/maple_story/guild.dart
+++ maple_story_book/lib/app/domain/repository/maple_story/guild.dart
@@ -1,3 +1,6 @@
+import 'package:maple_story_book/app/data/source/source.dart';
+import 'package:maple_story_book/app/domain/entity/entity.dart';
+
 ///
 /// @Project name    : maple_story_book
 /// @Class           : guild.
@@ -5,3 +8,10 @@
 /// Created On       : 2024. 9. 18..
 /// Description      : 
 ///
+
+abstract class IGuildRepository {
+  // 길드 식별자(oguild_id) 정보 조회
+  Future<ResultRest<GuildId>> getGuildId({required String guildName, required String worldName});
+  // 기본 정보 조회
+  Future<ResultRest<GuildId>> getGuildBasic({required String oGuildId, String? date});
+}
