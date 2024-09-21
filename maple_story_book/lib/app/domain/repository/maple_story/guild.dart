6a5e74e02b66e13bbdffffc90b52295291dbaeb7
dohy-9443 <dong_hyun95@hanmail.net>
2024-09-22 00:18:05 +0900

    feat : IRankingRepository 작성

--- maple_story_book/lib/app/domain/repository/maple_story/guild.dart
+++ maple_story_book/lib/app/domain/repository/maple_story/guild.dart
@@ -6,11 +6,12 @@ import 'package:maple_story_book/app/domain/entity/entity.dart';
 /// @Class           : guild.
 /// @Created by      : baekdonghyun.
 /// Created On       : 2024. 9. 18..
-/// Description      : 
+/// Description      : worldName enum 만들기
 ///
 
 abstract class IGuildRepository {
   // 길드 식별자(oguild_id) 정보 조회
+  /// TODO : worldName enum 만들기
   Future<ResultRest<GuildId>> getGuildId({required String guildName, required String worldName});
   // 기본 정보 조회
   Future<ResultRest<GuildId>> getGuildBasic({required String oGuildId, String? date});
--- maple_story_book/lib/app/domain/repository/maple_story/ranking.dart
+++ maple_story_book/lib/app/domain/repository/maple_story/ranking.dart
@@ -1,7 +1,30 @@
+import 'package:maple_story_book/app/data/source/source.dart';
+import 'package:maple_story_book/app/domain/entity/entity.dart';
+
 ///
 /// @Project name    : maple_story_book
 /// @Class           : ranking.
 /// @Created by      : baekdonghyun.
 /// Created On       : 2024. 9. 18..
-/// Description      : 
+/// Description      : rankingType, difficulty, class(availableValues) 직업 및 전직 enum 만들기
 ///
+
+
+
+abstract class IRankingRepository {
+  // 종합 랭킹 정보 조회
+  Future<ResultRest<RankingOverall>> getRankingOverall({ required String count, String? date, String? cursor });
+  // 유니온 랭킹 정보 조회
+  Future<ResultRest<RankingUnion>> getRankingUnion({ required String date, String? worldName, String? ocid, int? page });
+  // 길드 랭킹 정보 조회
+  /// TODO : rankingType enum 만들기
+  Future<ResultRest<RankingGuild>> getRankingGuild({ required String date, String? worldName, required int rankingType, String? guildName, int? page });
+  // 무릉도장 랭킹 정보 조회
+  /// TODO : difficulty enum 만들기
+  /// TODO : class(availableValues) 직업 및 전직 enum 만들기
+  Future<ResultRest<RankingStudio>> getRankingStudio({ required String date, String? worldName, required int difficulty, String? availableValue, String? ocid, int? page });
+  // 더 시드 랭킹 정보 조회
+  Future<ResultRest<RankingTheSeed>> getRankingTheSeed({ required String date, String? worldName, String? ocid, int? page });
+  // 업적 랭킹 정보 조회
+  Future<ResultRest<RankingAchievement>> getRankingAchievement({ required String date, String? worldName, String? ocid, int? page });
+}
