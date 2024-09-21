6a93f075e5077ee8d0b7fabc8ba302f9be497332
dohy-9443 <dong_hyun95@hanmail.net>
2024-09-21 23:32:39 +0900

    feat : IPotentialRepository 작성

--- maple_story_book/lib/app/domain/repository/maple_story/potential.dart
+++ maple_story_book/lib/app/domain/repository/maple_story/potential.dart
@@ -1,3 +1,6 @@
+import 'package:maple_story_book/app/data/source/source.dart';
+import 'package:maple_story_book/app/domain/entity/entity.dart';
+
 ///
 /// @Project name    : maple_story_book
 /// @Class           : potential.
@@ -5,3 +8,10 @@
 /// Created On       : 2024. 9. 18..
 /// Description      : 
 ///
+
+abstract class IPotentialRepository {
+  // 잠재능력 재설정 이용 결과 조회
+  Future<ResultRest<PotentialHistory>> getHistoryPotential({ required String count, String? date, String? cursor });
+  // 큐브 사용 결과 조회
+  Future<ResultRest<CubeHistory>> getHistoryCube({ required String count, String? date, String? cursor });
+}
