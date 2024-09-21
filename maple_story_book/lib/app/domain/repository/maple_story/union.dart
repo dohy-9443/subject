64e0ada95631f6162e7112e38e4b38f3bf6c28c1
dohy-9443 <dong_hyun95@hanmail.net>
2024-09-22 00:23:09 +0900

    feat : IUnionRepository 작성

--- maple_story_book/lib/app/domain/repository/maple_story/union.dart
+++ maple_story_book/lib/app/domain/repository/maple_story/union.dart
@@ -1,3 +1,6 @@
+import 'package:maple_story_book/app/data/source/source.dart';
+import 'package:maple_story_book/app/domain/entity/entity.dart';
+
 ///
 /// @Project name    : maple_story_book
 /// @Class           : union.
@@ -5,3 +8,12 @@
 /// Created On       : 2024. 9. 18..
 /// Description      : 
 ///
+
+abstract class IUnionRepository {
+  // 유니온 정보 조회
+  Future<ResultRest<UnionInfo>> getUserUnion({ required String ocid, String? date });
+  // 유니온 공격대 정보 조회
+  Future<ResultRest<UnionRaider>> getUserUnionRaider({ required String ocid, String? date });
+  // 유니온 아티팩트 정보 조회
+  Future<ResultRest<UnionArtifact>> getUserUnionArtifact({ required String ocid, String? date });
+}
