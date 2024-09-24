f049867044415ae9b4b97d77f94cfbd0a3c17d07
SHT-3756 <gkdlfndkekd@naver.com>
2024-09-24 17:09:09 +0900

    feat : enum 추가 (월드이름)

---
+++ maple_story_book/lib/core/enum/world_name.dart
@@ -0,0 +1,63 @@
+enum WorldName {
+  skania('스카니아'),
+  bera('베라'),
+  luna('루나'),
+  zenith('제니스'),
+  croa('크로아'),
+  union('유니온'),
+  elysium('엘리시움'),
+  enosis('이노시스'),
+  red('레드'),
+  aurora('오로라'),
+  arcane('아케인'),
+  nova('노바'),
+  reboot('리부트1'),
+  reboot2('리부트2'),
+  bunning('버닝1'),
+  bunning2('버닝2'),
+  bunning3('버닝3');
+
+  final String name;
+
+  const WorldName(this.name);
+
+  static WorldName getWorldName(String name) {
+    switch (name) {
+      case 'skania':
+        return WorldName.skania;
+      case 'bera':
+        return WorldName.bera;
+      case 'zenith':
+        return WorldName.zenith;
+      case 'croa':
+        return WorldName.croa;
+      case 'union':
+        return WorldName.union;
+      case 'elysium':
+        return WorldName.elysium;
+      case 'enosis':
+        return WorldName.enosis;
+      case 'red':
+        return WorldName.red;
+      case 'aurora':
+        return WorldName.aurora;
+      case 'arcane':
+        return WorldName.arcane;
+      case 'nova':
+        return WorldName.nova;
+      case 'reboot':
+        return WorldName.reboot;
+      case 'reboot2':
+        return WorldName.reboot2;
+      case 'bunning':
+        return WorldName.bunning;
+      case 'bunning2':
+        return WorldName.bunning2;
+      case 'bunning3':
+        return WorldName.bunning3;
+
+      default:
+        throw Exception('Invalid WorldName : $name');
+    }
+  }
+}
