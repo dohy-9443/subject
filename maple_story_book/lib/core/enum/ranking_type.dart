a14e9bcc4a7bc1ddaa60c678de31942133626e83
SHT-3756 <gkdlfndkekd@naver.com>
2024-09-24 17:09:41 +0900

    feat : enum 추가 (랭킹타입)

---
+++ maple_story_book/lib/core/enum/ranking_type.dart
@@ -0,0 +1,25 @@
+enum RankingType {
+  zero(num: 0, name: '주간 명성치'),
+  one(num: 1, name: '플래그 레이스'),
+  two(num: 2, name: '지하 수로');
+
+  final int num;
+  final String name;
+
+  const RankingType({required this.num, required this.name});
+
+  static RankingType getRankingType(String name) {
+    switch (name) {
+      case 'zero':
+        return RankingType.zero;
+      case 'one':
+        return RankingType.one;
+      case 'two':
+        return RankingType.two;
+      default:
+        throw Exception('Invalid RankingType : $name');
+    }
+  }
+}
+
+
