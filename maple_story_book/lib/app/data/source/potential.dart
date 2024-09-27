b8b0f7fdbbb37bfb41201fd3bd71a7e76ab5e187
SHT-3756 <gkdlfndkekd@naver.com>
2024-09-27 22:06:26 +0900

    feat : 잠재능력 repository, source 생성

---
+++ maple_story_book/lib/app/data/source/potential.dart
@@ -0,0 +1,31 @@
+import 'package:maple_story_book/tool/network/api/api.dart';
+
+///
+/// @Project name    : maple_story_book
+/// @Class           : potential.
+/// @Created by      : shinheetae.
+/// Created On       : 2024. 9. 27..
+/// Description      :
+///
+
+abstract class IGetPotentialDataSource {
+  getHistoryPotential({required String count, DateTime? date, String? cursor});
+  getHistoryCube({required String count, DateTime? date, String? cursor});
+}
+
+class PotentialDataSource implements IGetPotentialDataSource {
+  final GetMapleStoryBookPotentialApi _api;
+
+  PotentialDataSource(this._api);
+
+  @override
+  getHistoryCube({required String count, DateTime? date, String? cursor}) {
+    return _api.getHistoryCube(count: count, date: date, cursor: cursor);
+  }
+
+  @override
+  getHistoryPotential({required String count, DateTime? date, String? cursor}) {
+    return _api.getHistoryPotential(count: count, date: date, cursor: cursor);
+  }
+}
+
