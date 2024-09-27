336ae375ee8cfae108c28471320268052780dec5
dohy-9443 <dong_hyun95@hanmail.net>
2024-09-27 18:47:37 +0900

    feat : GetMapleStoryBookPotentialApi 작성

--- maple_story_book/lib/tool/network/api/get_maple_story_book_potential.dart
+++ maple_story_book/lib/tool/network/api/get_maple_story_book_potential.dart
@@ -1,3 +1,6 @@
+import 'package:maple_story_book/core/util/util.dart';
+import 'package:maple_story_book/tool/network/network.dart';
+
 ///
 /// @Project name    : maple_story_book
 /// @Class           : get_maple_story_book_potential.
@@ -5,3 +8,33 @@
 /// Created On       : 2024. 9. 27..
 /// Description      : 
 ///
+
+class GetMapleStoryBookPotentialApi {
+  final MapleStoryBookRestClient _client;
+
+  GetMapleStoryBookPotentialApi(this._client);
+
+  static String defaultPath = '/maplestory/v1/history';
+
+  Future<dynamic> getHistoryPotential({required String count, DateTime? date, String? cursor}) {
+    return _client.get<dynamic>(
+      '$defaultPath/potential',
+      queryParameters: {
+        'count' : count,
+        'date' : dateToString(date),
+        'cursor' : cursor,
+      },
+    ).then((value) => value.data);
+  }
+
+  Future<dynamic> getHistoryCube({required String count, DateTime? date, String? cursor}) {
+    return _client.get<dynamic>(
+      '$defaultPath/cube',
+      queryParameters: {
+        'count' : count,
+        'date' : dateToString(date),
+        'cursor' : cursor,
+      },
+    ).then((value) => value.data);
+  }
+}
