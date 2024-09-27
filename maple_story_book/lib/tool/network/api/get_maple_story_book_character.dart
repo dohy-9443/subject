cf73cc583c9544a60d47e4a434a31583a58000c8
dohy-9443 <dong_hyun95@hanmail.net>
2024-09-27 18:30:23 +0900

    feat : GetMapleStoryBookCharacterApi 작성

--- maple_story_book/lib/tool/network/api/get_maple_story_book_character.dart
+++ maple_story_book/lib/tool/network/api/get_maple_story_book_character.dart
@@ -16,6 +16,7 @@ class GetMapleStoryBookCharacterApi {
   const GetMapleStoryBookCharacterApi(this._client);
 
   static String defaultPath = '/maplestory/v1';
+  static String defaultCharacterPath = '$defaultPath/character';
 
   Future<dynamic> getOcid({required String characterName}) {
     return _client.get<dynamic>('$defaultPath/id', queryParameters: {
@@ -23,12 +24,193 @@ class GetMapleStoryBookCharacterApi {
     }).then((value) => value.data);
   }
 
+  Future<dynamic> getCharacterBasic({required String ocid, DateTime? date}) {
+    return _client.get<dynamic>(
+      '$defaultCharacterPath/basic',
+      queryParameters: {
+        'ocid' : ocid,
+        'date' : dateToString(date)
+      },
+    ).then((value) => value.data);
+  }
+
+  Future<dynamic> getCharacterPopularity({required String ocid, DateTime? date}) {
+    return _client.get<dynamic>(
+      '$defaultCharacterPath/popularity',
+      queryParameters: {
+        'ocid' : ocid,
+        'date' : dateToString(date)
+      },
+    ).then((value) => value.data);
+  }
+
+  Future<dynamic> getCharacterStat({required String ocid, DateTime? date}) {
+    return _client.get<dynamic>(
+      '$defaultCharacterPath/stat',
+      queryParameters: {
+        'ocid' : ocid,
+        'date' : dateToString(date)
+      },
+    ).then((value) => value.data);
+  }
+
+  Future<dynamic> getCharacterHyperStat({required String ocid, DateTime? date}) {
+    return _client.get<dynamic>(
+      '$defaultCharacterPath/hyper-stat',
+      queryParameters: {
+        'ocid' : ocid,
+        'date' : dateToString(date)
+      },
+    ).then((value) => value.data);
+  }
+
+  Future<dynamic> getCharacterPropensity({required String ocid, DateTime? date}) {
+    return _client.get<dynamic>(
+      '$defaultCharacterPath/propensity',
+      queryParameters: {
+        'ocid' : ocid,
+        'date' : dateToString(date)
+      },
+    ).then((value) => value.data);
+  }
+
   Future<dynamic> getCharacterAbility({required String ocid, DateTime? date}) {
 
-    return _client.get<dynamic>('$defaultPath/character/ability',
+    return _client.get<dynamic>('$defaultCharacterPath/ability',
       queryParameters: {
         'ocid': ocid,
         'date': dateToString(date)
+      },
+    ).then((value) => value.data);
+  }
+
+  Future<dynamic> getCharacterItemEquipment({required String ocid, DateTime? date}) {
+    return _client.get<dynamic>(
+      '$defaultCharacterPath/item-equipment',
+      queryParameters: {
+        'ocid' : ocid,
+        'date' : dateToString(date)
+      },
+    ).then((value) => value.data);
+  }
+
+  Future<dynamic> getCharacterCashItemEquipment({required String ocid, DateTime? date}) {
+    return _client.get<dynamic>(
+      '$defaultCharacterPath/cashitem-equipment',
+      queryParameters: {
+        'ocid' : ocid,
+        'date' : dateToString(date)
+      },
+    ).then((value) => value.data);
+  }
+
+  Future<dynamic> getCharacterSymbolEquipment({required String ocid, DateTime? date}) {
+    return _client.get<dynamic>(
+      '$defaultCharacterPath/symbol-equipment',
+      queryParameters: {
+        'ocid' : ocid,
+        'date' : dateToString(date)
+      },
+    ).then((value) => value.data);
+  }
+
+  Future<dynamic> getCharacterSetEffect({required String ocid, DateTime? date}) {
+    return _client.get<dynamic>(
+      '$defaultCharacterPath/set-effect',
+      queryParameters: {
+        'ocid' : ocid,
+        'date' : dateToString(date)
+      },
+    ).then((value) => value.data);
+  }
+
+  Future<dynamic> getCharacterBeautyEquipment({required String ocid, DateTime? date}) {
+    return _client.get<dynamic>(
+      '$defaultCharacterPath/beauty-equipment',
+      queryParameters: {
+        'ocid' : ocid,
+        'date' : dateToString(date)
+      },
+    ).then((value) => value.data);
+  }
+
+  Future<dynamic> getCharacterAndroidEquipment({required String ocid, DateTime? date}) {
+    return _client.get<dynamic>(
+      '$defaultCharacterPath/android-equipment',
+      queryParameters: {
+        'ocid' : ocid,
+        'date' : dateToString(date)
+      },
+    ).then((value) => value.data);
+  }
+
+  Future<dynamic> getCharacterPetEquipment({required String ocid, DateTime? date}) {
+    return _client.get<dynamic>(
+      '$defaultCharacterPath/pet-equipment',
+      queryParameters: {
+        'ocid' : ocid,
+        'date' : dateToString(date)
+      },
+    ).then((value) => value.data);
+  }
+
+  Future<dynamic> getCharacterSkill({required String ocid, DateTime? date, required String characterSkillGrade}) {
+    return _client.get<dynamic>(
+      '$defaultCharacterPath/skill',
+      queryParameters: {
+        'ocid' : ocid,
+        'date' : dateToString(date),
+        'character_skill_grade' : characterSkillGrade,
+      },
+    ).then((value) => value.data);
+  }
+
+  Future<dynamic> getCharacterLinkSkill({required String ocid, DateTime? date}) {
+    return _client.get<dynamic>(
+      '$defaultCharacterPath/link-skill',
+      queryParameters: {
+        'ocid' : ocid,
+        'date' : dateToString(date)
+      },
+    ).then((value) => value.data);
+  }
+
+  Future<dynamic> getCharacterVMatrix({required String ocid, DateTime? date}) {
+    return _client.get<dynamic>(
+      '$defaultCharacterPath/vmatrix',
+      queryParameters: {
+        'ocid' : ocid,
+        'date' : dateToString(date)
+      },
+    ).then((value) => value.data);
+  }
+
+  Future<dynamic> getCharacterHexamatrix({required String ocid, DateTime? date}) {
+    return _client.get<dynamic>(
+      '$defaultCharacterPath/hexamatrix',
+      queryParameters: {
+        'ocid' : ocid,
+        'date' : dateToString(date)
+      },
+    ).then((value) => value.data);
+  }
+
+  Future<dynamic> getCharacterHexamatrixStat({required String ocid, DateTime? date}) {
+    return _client.get<dynamic>(
+      '$defaultCharacterPath/hexamatrix-stat',
+      queryParameters: {
+        'ocid' : ocid,
+        'date' : dateToString(date)
+      },
+    ).then((value) => value.data);
+  }
+
+  Future<dynamic> getCharacterStudio({required String ocid, DateTime? date}) {
+    return _client.get<dynamic>(
+      '$defaultCharacterPath/dojang',
+      queryParameters: {
+        'ocid' : ocid,
+        'date' : dateToString(date)
       },
     ).then((value) => value.data);
   }
