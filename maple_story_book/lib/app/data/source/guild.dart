80b7d1eecb6c64885784f5ec699a20cd12e8720f
dohy-9443 <dong_hyun95@hanmail.net>
2024-09-27 21:57:48 +0900

    feat : GuildDataSource 작성

--- maple_story_book/lib/app/data/source/guild.dart
+++ maple_story_book/lib/app/data/source/guild.dart
@@ -1,3 +1,5 @@
+import 'package:maple_story_book/tool/network/network.dart';
+
 ///
 /// @Project name    : maple_story_book
 /// @Class           : guild.
@@ -9,4 +11,20 @@
 abstract class IGetGuildDataSource {
   Future<dynamic> getGuildId({required String guildName, required String worldName});
   Future<dynamic> getGuildBasic({required String oguildId, DateTime? date});
+}
+
+class GuildDataSource implements IGetGuildDataSource {
+  final GetMapleStoryBookGuildApi _api;
+
+  GuildDataSource(this._api);
+
+  @override
+  Future getGuildBasic({required String oguildId, DateTime? date}) {
+    return _api.getGuildBasic(oguildId: oguildId, date: date);
+  }
+
+  @override
+  Future getGuildId({required String guildName, required String worldName}) {
+    return _api.getGuildId(guildName: guildName, worldName: worldName);
+  }
 }
