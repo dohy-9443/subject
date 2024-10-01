145f60e1f14e34e093d83ad00ef62cf9d118b8f3
dohy-9443 <dong_hyun95@hanmail.net>
2024-10-01 21:34:38 +0900

    feat : search screen image add

--- maple_story_book/lib/app/presentation/search/seach_screen.dart
+++ maple_story_book/lib/app/presentation/search/seach_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maple_story_book/core/util/insets.dart';
 import 'package:maple_story_book/tool/component/component.dart';
 import 'package:maple_story_book/tool/theme/theme.dart';
 
@@ -17,12 +18,26 @@ class SearchScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: ColorName.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          MSText.taengGu('하이', fontSize: 40,)
-        ],
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: AppInset.edgeInsetsValue([50, 0, 0, 0]),
+              child: Container(
+                width: 300,
+                height: 300,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage(Assets.images.slime.path),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
