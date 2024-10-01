b1e15f9fb8ca37903b1081953415079cbd4b7bea
dohy-9443 <dong_hyun95@hanmail.net>
2024-10-01 22:50:17 +0900

    feat : leaf animation add

--- maple_story_book/lib/app/presentation/search/seach_screen.dart
+++ maple_story_book/lib/app/presentation/search/seach_screen.dart
@@ -1,5 +1,5 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:maple_story_book/core/util/insets.dart';
 import 'package:maple_story_book/tool/component/component.dart';
 import 'package:maple_story_book/tool/theme/theme.dart';
 
@@ -11,34 +11,145 @@ import 'package:maple_story_book/tool/theme/theme.dart';
 /// Description      : 
 ///
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
 
   @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  final Random _random = Random();
+  List<_LeafAnimation> _leafAnimations = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 130),
+      vsync: this,
+    )..repeat();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _initializeLeafAnimations(context);
+    });
+  }
+
+  void _initializeLeafAnimations(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    setState(() {
+      _leafAnimations = List.generate(70, (index) {
+        return _LeafAnimation(
+          left: _random.nextDouble() * screenWidth,
+          top: -_random.nextDouble() * 100,
+          size: 30 + _random.nextDouble() * 50,
+          speed: 5,
+          delay: 30 + _random.nextDouble() * 50,
+          animationController: _animationController,
+          screenWidth: screenWidth,
+          screenHeight: screenHeight,
+          random: _random,
+        );
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: ColorName.white,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: AppInset.edgeInsetsValue([50, 0, 0, 0]),
-              child: Container(
-                width: 300,
-                height: 300,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage(Assets.images.slime.path),
-                    fit: BoxFit.contain,
+    _initializeLeafAnimations(context);
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(color: ColorName.mainAccent,),
+          ..._leafAnimations.map((leaf) => leaf.build(context)).toList(),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: MSText.taengGu(
+                    '캐릭터의 닉네임을\n 입력해주세요.',
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    textAlign: TextAlign.center,
                   ),
                 ),
-              ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(Assets.images.orange.path),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class _LeafAnimation {
+  final double left;
+  final double top;
+  final double size;
+  final double speed;
+  final double delay;
+  final double screenWidth;
+  final double screenHeight;
+  final AnimationController animationController;
+  final Random random;
+
+  _LeafAnimation({
+    required this.left,
+    required this.top,
+    required this.size,
+    required this.speed,
+    required this.delay,
+    required this.animationController,
+    required this.screenWidth,
+    required this.screenHeight,
+    required this.random,
+  });
+
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: animationController,
+      builder: (context, child) {
+        final animationValue = (animationController.value + delay) % 1.0;
+        final dx = 0;
+        final dy = (speed * animationValue * screenHeight);
+        final newTop = top + dy;
+        final newLeft = (left + dx) % screenWidth;
+
+        return Positioned(
+          left: newLeft,
+          top: newTop,
+          child: Transform.rotate(
+            angle: animationValue * 2 * pi,
+            child: Image.asset(
+              Assets.icons.logoIcon.path,
+              width: size,
+              height: size,
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- maple_story_book/lib/tool/component/maple_story_text.dart
+++ maple_story_book/lib/tool/component/maple_story_text.dart
@@ -18,6 +18,7 @@ class MSText extends StatelessWidget {
   final FontWeight fontWeight;
   final double fontSize;
   final bool isGoogleFont;
+  final TextAlign textAlign;
 
   const MSText(
     this.text, {
@@ -26,28 +27,31 @@ class MSText extends StatelessWidget {
       this.fontWeight = FontWeight.normal,
       this.fontSize = 16,
       this.isGoogleFont = true,
+      this.textAlign = TextAlign.left,
     }
   );
 
-  factory MSText.basic(String text, {double fontSize = 16}) => MSText(text, fontSize: fontSize,);
+  factory MSText.basic(String text, {double fontSize = 16, TextAlign textAlign = TextAlign.left,}) => MSText(text, fontSize: fontSize, textAlign: textAlign,);
 
-  factory MSText.bold(String text, {double fontSize = 16}) => MSText(text, fontWeight: FontWeight.bold, fontSize: fontSize,);
+  factory MSText.bold(String text, {double fontSize = 16, TextAlign textAlign = TextAlign.left,}) => MSText(text, fontWeight: FontWeight.bold, fontSize: fontSize, textAlign: textAlign,);
 
-  factory MSText.danger(String text, {double fontSize = 16}) => MSText(text, color: ColorName.danger, fontSize: fontSize,);
+  factory MSText.danger(String text, {double fontSize = 16, TextAlign textAlign = TextAlign.left,}) => MSText(text, color: ColorName.danger, fontSize: fontSize, textAlign: textAlign,);
 
   factory MSText.taengGu(
     String text, {
       double fontSize = 16,
       FontWeight fontWeight = FontWeight.normal,
       Color color = ColorName.black,
+      TextAlign textAlign = TextAlign.left,
       }) {
-    return MSText(text, fontSize: fontSize, fontWeight: fontWeight, color: color, isGoogleFont: false);
+    return MSText(text, fontSize: fontSize, fontWeight: fontWeight, color: color, isGoogleFont: false, textAlign: textAlign);
   }
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
+      textAlign: textAlign,
       style: isGoogleFont ? GoogleFonts.roboto(
         fontSize: fontSize,
         fontWeight: fontWeight,
