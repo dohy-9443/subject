1edb5307835e15b53e0a34984b552205d157ea88
SHT-3756 <gkdlfndkekd@naver.com>
2024-10-06 14:55:37 +0900

    feat : 시간 컨버트 코드 추가

--- maple_story_book/lib/core/util/json.dart
+++ maple_story_book/lib/core/util/json.dart
@@ -41,3 +41,13 @@ String dateToString(DateTime? date) {
 
   return DateFormat('yyyy-MM-dd').format(date ?? yesterday);
 }
+
+String formatSeconds(int seconds) {
+  int minutes = seconds ~/ 60;
+  int remainingSeconds = seconds % 60;
+
+  String minutesStr = minutes.toString().padLeft(2, '0');
+  String secondsStr = remainingSeconds.toString().padLeft(2, '0');
+
+  return "$minutesStr분 $secondsStr초";
+}
