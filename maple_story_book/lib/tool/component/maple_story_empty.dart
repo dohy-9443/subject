5a7fbc933dd4f6873c024ae7805a0fe702760578
SHT-3756 <gkdlfndkekd@naver.com>
2024-10-07 16:32:02 +0900

    feat : empty 화면 추가

---
+++ maple_story_book/lib/tool/component/maple_story_empty.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:maple_story_book/tool/theme/theme.dart';
+import 'package:maple_story_book/tool/widget/widget.dart';
+
+///
+/// @Project name    : maple_story_book
+/// @Class           : maple_story_empty.
+/// @Created by      : shinheetae.
+/// Created On       : 2024. 10. 7..
+/// Description      :
+///
+
+class MsEmpty extends StatelessWidget {
+  String? description;
+  MsEmpty({super.key, this.description});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 300,
+              height: 300,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(Assets.images.yetti.path),
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            MSText.bold(description ?? '리스트가 비어있습니다.', color: ColorName.darkGray2,),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
