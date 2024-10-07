e602e48d63e98c1b895b2f4b2f3142e4092cef9b
SHT-3756 <gkdlfndkekd@naver.com>
2024-10-08 00:13:46 +0900

    fix :  home_event 파라미터 수정

--- maple_story_book/lib/app/presentation/home/bloc/home_event.dart
+++ maple_story_book/lib/app/presentation/home/bloc/home_event.dart
@@ -11,27 +11,21 @@ import 'package:equatable/equatable.dart';
 sealed class IHomeEvent<T> extends Equatable {}
 
 final class GetHomeEvent<T> extends IHomeEvent<T> {
-  final String _ocid;
+  final String ocid;
   final DateTime? date;
 
-  GetHomeEvent(this._ocid, {this.date});
-
-  String get ocid => _ocid;
+  GetHomeEvent({required this.ocid, this.date});
 
   @override
   List<Object?> get props => [ocid, date];
 }
 
 final class GetSkillEvent extends IHomeEvent {
-  final String _ocid;
-  final DateTime _date;
-  final String _characterSkillGrade;
+  final String ocid;
+  final DateTime? date;
+  final String characterSkillGrade;
 
-  GetSkillEvent(this._ocid, this._date, this._characterSkillGrade);
-
-  String get ocid => _ocid;
-  DateTime get date => _date;
-  String get characterSkillGrade => _characterSkillGrade;
+  GetSkillEvent({required this.ocid, this.date, required this.characterSkillGrade});
 
   @override
   List<Object?> get props => [ocid, date, characterSkillGrade];
