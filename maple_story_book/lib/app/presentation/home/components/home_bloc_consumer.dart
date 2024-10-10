4ab38d85c8f38b968653cc0145e85e8120a240b1
dohy-9443 <dong_hyun95@hanmail.net>
2024-10-10 17:51:41 +0900

    refactor : Home Screen , Home Bloc Consumer 수정

--- maple_story_book/lib/app/presentation/home/components/home_bloc_consumer.dart
+++ maple_story_book/lib/app/presentation/home/components/home_bloc_consumer.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:maple_story_book/app/domain/entity/entity.dart';
 import 'package:maple_story_book/app/presentation/home/bloc/home_bloc.dart';
+import 'package:maple_story_book/app/presentation/home/bloc/home_event.dart';
 import 'package:maple_story_book/app/presentation/home/bloc/home_state.dart';
 import 'package:maple_story_book/app/presentation/home/components/components.dart';
-import 'package:maple_story_book/core/util/bloc/base_state.dart';
-import 'package:maple_story_book/tool/component/component.dart';
+import 'package:maple_story_book/core/util/util.dart';
 
 ///
 /// @Project name    : maple_story_book
@@ -18,20 +19,16 @@ class HomeBlocConsumer extends StatelessWidget {
   const HomeBlocConsumer({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return BlocConsumer<HomeBloc, IHomeState>(
-      listener: (BuildContext context, IHomeState state) {
+  Widget build(BuildContext context) => MSBlocConsumer<HomeBloc, IHomeState>(
+    initFunc: initFunction,
+    bloc: context.read<HomeBloc>(),
+    success: (context, successState) => HomeSuccessWidget(),
+    errorPressed: () {},
+    errorFullScreenPressed: () {}
+  );
 
-      },
-      builder: (BuildContext context, IHomeState state) {
-        return Container();
-        // return state.when<Widget>(
-        //   initial: () => MSLoading(),
-        //   success: (data) => HomeSuccessWidget(data: data as HomeData),
-        //   error: (error, StackTrace? stackTrace) => HomeErrorWidget(error: error),
-        //   // orElse: () => Container(),
-        // );
-      },
-    );
+  initFunction(BuildContext context) {
+    context.read<HomeBloc>().add(GetHomeEvent<BasicInfo>(ocid: "36c54981582f63ef732f51a1216299d2"));
   }
 }
+
--- maple_story_book/lib/app/presentation/home/home_screen.dart
+++ maple_story_book/lib/app/presentation/home/home_screen.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:maple_story_book/app/domain/entity/entity.dart';
-import 'package:maple_story_book/app/presentation/home/bloc/home_bloc.dart';
-import 'package:maple_story_book/app/presentation/home/bloc/home_event.dart';
 import 'package:maple_story_book/app/presentation/home/components/home_bloc_consumer.dart';
 
 ///
@@ -13,25 +9,10 @@ import 'package:maple_story_book/app/presentation/home/components/home_bloc_cons
 /// Description      :
 ///
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-
-  @override
-  void initState() {
-    context.read<HomeBloc>().add(GetHomeEvent<BasicInfo>(ocid: "36c54981582f63ef732f51a1216299d2"));
-    // context.read<RankingBloc>().add(GetRankingStudioEvent<RankingStudio>(
-    //   date: DateTime.now().subtract(const Duration(days: 2)),
-    //   difficulty: 1,
-    // ));
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) => HomeBlocConsumer();
 }
+
