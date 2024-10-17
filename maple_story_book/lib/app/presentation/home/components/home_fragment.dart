87bc3eaea8375132d92fada3243bbb9d5159ef35
dohy-9443 <dong_hyun95@hanmail.net>
2024-10-17 21:41:28 +0900

    design : home screen ui changed

--- maple_story_book/lib/app/presentation/home/components/home_fragment.dart
+++ maple_story_book/lib/app/presentation/home/components/home_fragment.dart
@@ -3,6 +3,7 @@ import 'package:maple_story_book/app/domain/entity/entity.dart';
 import 'package:maple_story_book/app/presentation/home/bloc/home_state.dart';
 import 'package:maple_story_book/app/presentation/home/components/components.dart';
 import 'package:maple_story_book/core/util/util.dart';
+import 'package:maple_story_book/tool/theme/theme.dart';
 import 'package:maple_story_book/tool/widget/widget.dart';
 
 ///
@@ -29,18 +30,33 @@ class _HomeFragmentState extends State<HomeFragment> with SingleTickerProviderSt
 
   late TabController _tabController;
   ScrollController scrollController = ScrollController();
+  ScrollController tabScrollController = ScrollController();
+  bool isExpanded = true;
 
   @override
   void initState() {
     super.initState();
+    scrollController.addListener(scrollListener);
     _tabController = TabController(length: 4, vsync: this);
   }
 
   @override
   void dispose() {
     _tabController.dispose();
+    scrollController.removeListener(scrollListener);
+    tabScrollController.dispose();
     scrollController.dispose();
     super.dispose();
+  }
+
+  scrollListener() {
+    bool isCurrentlyExpanded = scrollController.offset < 200 - kToolbarHeight;
+
+    if (isExpanded != isCurrentlyExpanded) {
+      setState(() {
+        isExpanded = isCurrentlyExpanded;
+      });
+    }
   }
 
   @override
@@ -50,54 +66,99 @@ class _HomeFragmentState extends State<HomeFragment> with SingleTickerProviderSt
     HyperStat hyperStat = widget.state.hyperStat;
     Ability ability = widget.state.ability;
 
-    return MSBaseBuildWidget(
-      title: basicInfo.characterName,
-      body: Container(
-        child: SingleChildScrollView(
+    return CustomScrollView(
+      controller: scrollController,
+      slivers: [
+        SliverAppBar(
+          pinned: true,
+          expandedHeight: 200,
+          backgroundColor: isExpanded ? ColorName.lightBg : ColorName.mainAccent,
+          floating: true,
+          centerTitle: true,
+          title: isExpanded ? null : MSText.bold(basicInfo.characterName),
+          flexibleSpace: FlexibleSpaceBar(
+            background: SafeArea(
+              child: Padding(
+                padding: AppInset.h8,
+                child: HomeTop(basicInfo: basicInfo,),
+              ),
+            ),
+          )
+        ),
+        SliverToBoxAdapter(child: AppSize.height(16),),
+        SliverPersistentHeader(
+          pinned: true,
+          delegate: _SliverTabBarDelegate(MSTabBar(
+            scrollController: tabScrollController,
+            tabController: _tabController,
+            tabBarList: [
+              TabBarType(
+                onTap: () {},
+                text: '스텟',
+              ),
+              TabBarType(
+                onTap: () {},
+                text: '장비',
+              ),
+              TabBarType(
+                onTap: () {},
+                text: '스킬',
+              ),
+              TabBarType(
+                onTap: () {},
+                text: '성향 / 어빌리티',
+              ),
+            ],
+          )),
+        ),
+        SliverToBoxAdapter(child: AppSize.height(8),),
+        SliverToBoxAdapter(
           child: Padding(
             padding: AppInset.all8,
-            child: Column(
-              children: [
-                HomeTop(basicInfo: basicInfo,),
-                AppSize.height(8),
-                MSTabBar(
-                  scrollController: scrollController,
-                  tabController: _tabController,
-                  tabBarList: [
-                    TabBarType(
-                      onTap: () {},
-                      text: '스텟',
-                    ),
-                    TabBarType(
-                      onTap: () {},
-                      text: '장비',
-                    ),
-                    TabBarType(
-                      onTap: () {},
-                      text: '스킬',
-                    ),
-                    TabBarType(
-                      onTap: () {},
-                      text: '성향 / 어빌리티',
-                    ),
-                  ],
-                ),
-                AppSize.height(8),
-
-                TabBarView(
-                  controller: _tabController,
-                  children: [
-                    HomeCenter(stat: stat, hyperStat: hyperStat, ability: ability),
-                    Container(color: Colors.redAccent,),
-                    Container(color: Colors.orangeAccent,),
-                    Container(color: Colors.yellowAccent,),
-                  ],
-                ),
-              ],
-            ),
+            child: HomeCenter(stat: stat, hyperStat: hyperStat, ability: ability),
           ),
         ),
-      ),
+      ],
     );
   }
 }
+
+class _SliverTabBarDelegate extends SliverPersistentHeaderDelegate {
+  final MSTabBar tabBar;
+
+  _SliverTabBarDelegate(this.tabBar);
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: Colors.white, // MSTabBar의 배경 색상
+      child: PreferredSize(
+        preferredSize: Size.fromHeight(48), // MSTabBar의 높이를 설정
+        child: tabBar,
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => 48;
+
+  @override
+  double get minExtent => 48;
+
+  @override
+  bool shouldRebuild(_SliverTabBarDelegate oldDelegate) {
+    return oldDelegate.tabBar != tabBar;
+  }
+}
+
+// SingleChildScrollView(
+//   child: TabBarView(
+//     controller: _tabController,
+//     children: [
+//       HomeCenter(stat: stat, hyperStat: hyperStat, ability: ability),
+//       Container(color: Colors.redAccent,),
+//       Container(color: Colors.orangeAccent,),
+//       Container(color: Colors.yellowAccent,),
+//     ],
+//   ),
+// ),
