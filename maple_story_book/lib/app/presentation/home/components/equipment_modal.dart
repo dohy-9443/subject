a8da887a596e419f897c1a94e33047d4fee60b09
dohy-9443 <dong_hyun95@hanmail.net>
2024-11-20 16:44:57 +0900

    design : equipment modal star force 작업

--- maple_story_book/lib/app/presentation/home/components/equipment_modal.dart
+++ maple_story_book/lib/app/presentation/home/components/equipment_modal.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:maple_story_book/app/domain/entity/entity.dart';
 import 'package:maple_story_book/core/extension/build_context_extension.dart';
+import 'package:maple_story_book/core/util/util.dart';
 import 'package:maple_story_book/tool/theme/theme.dart';
 
 ///
@@ -18,6 +19,12 @@ class EquipmentModal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print("item.starforce : ${item.starforce}");
+
+    final starSize = (context.screenWidth - 14 * 5) / 15;
+    const int starsPerRow = 15; // 한 줄에 배치할 별 개수
+    const double spacing = 5.0; // 별 사이 간격
+
     return Container(
       width: context.screenWidth * .8,
       height: context.screenHeight * .8,
@@ -27,7 +34,32 @@ class EquipmentModal extends StatelessWidget {
         border: Border.all(
           color: ColorName.white,
           width: 2,
-        )
+        ),
+      ),
+      child: Padding(
+        padding: AppInset.all8,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+              child: Wrap(
+                runSpacing: 10,
+                alignment: WrapAlignment.center,
+                children: List.generate(25, (index) {
+                  return SizedBox(
+                    width: (index + 1) % 5 == 0 ? starSize : starSize * 0.8,
+                    height: starSize * 0.8,
+                    child: Icon(
+                      Icons.star,
+                      color: index < item.starforce ? Colors.yellowAccent : Colors.grey,
+                      size: starSize * 0.8,
+                    ),
+                  );
+                }),
+              )
+            )
+          ],
+        ),
       ),
     );
   }
